73cf5d68679a5cd7ae6ad1292b7a1e10d6f49e91
Vitamax Oduol <85936735+vitamaxoduol@users.noreply.github.com>
2023-06-19 00:35:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const mainElement = document.querySelector('main'); // Remove the <main> element with id 'main'
+mainElement.remove();
+
+const newHeader = document.createElement('h1'); // Create a new <h1> element with id 'victory'
+newHeader.id = 'victory';
+
+newHeader.textContent = 'YOUR-NAME is the champion'; // Set the text content of the new <h1> element
+
+
+document.body.appendChild(newHeader);// Append the new <h1> element to the document body
